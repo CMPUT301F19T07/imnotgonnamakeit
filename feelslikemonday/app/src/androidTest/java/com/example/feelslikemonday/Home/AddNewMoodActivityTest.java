b548d9fd2c314ccdec995bda36dee0547a218eee
relhajj <relhajj@users.noreply.github.com>
2019-11-07 12:26:07 -0700

    Added adroid test dependencies and created home page test class

---
+++ feelslikemonday/app/src/androidTest/java/com/example/feelslikemonday/Home/AddNewMoodActivityTest.java
@@ -0,0 +1,30 @@
+package com.example.feelslikemonday.Home;
+
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.*;
+
+/**
+ * Test class for MainActivity. All the UI tests are written here.
+ * Robotium test framework is used
+ */
+
+public class AddNewMoodActivityTest {
+}
