6f5f12d9d438d904a83b30a2fb8caf0c2cd9c45d
relhajj <relhajj@users.noreply.github.com>
2019-11-07 12:36:01 -0700

    added skeleton tests for add,edit,delete,view moods

--- feelslikemonday/app/src/androidTest/java/com/example/feelslikemonday/Home/AddNewMoodActivityTest.java
+++ feelslikemonday/app/src/androidTest/java/com/example/feelslikemonday/Home/AddNewMoodActivityTest.java
@@ -11,6 +11,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.example.feelslikemonday.ui.login.LoginMainActivity;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -22,9 +23,51 @@ import org.junit.runner.RunWith;
 import static junit.framework.TestCase.*;
 
 /**
- * Test class for MainActivity. All the UI tests are written here.
+ * Test class for AddNewMoodActivity. All the UI tests are written here.
  * Robotium test framework is used
  */
-
+@RunWith(AndroidJUnit4.class)
 public class AddNewMoodActivityTest {
+
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<LoginMainActivity> rule =
+            new ActivityTestRule<>(LoginMainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp()throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+
+    /**
+     * Gets the activity
+     * @throws Exception
+     */
+    @Test
+    public void start()throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+    @Test
+    public  void addMoodTest(){
+    }
+
+    @Test
+    public  void editMoodTest(){
+    }
+
+    @Test
+    public  void deleteMoodTest(){
+    }
+
+    @Test
+    public  void viewMoodTest(){
+    }
+
 }
+
