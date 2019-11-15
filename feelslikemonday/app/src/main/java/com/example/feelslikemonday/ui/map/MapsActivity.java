475033d3b8439ad900347466d6bd8c3ad54f8a7c
Yuning <37944809+Starscream29@users.noreply.github.com>
2019-11-14 19:12:33 -0700

    map displays markers for moods, color coded according to the main screen

--- feelslikemonday/app/src/main/java/com/example/feelslikemonday/ui/map/MapsActivity.java
+++ feelslikemonday/app/src/main/java/com/example/feelslikemonday/ui/map/MapsActivity.java
@@ -1,20 +1,46 @@
 package com.example.feelslikemonday.ui.map;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.example.feelslikemonday.DAO.UserCallback;
+import com.example.feelslikemonday.DAO.UserDAO;
+import com.example.feelslikemonday.DAO.VoidCallback;
 import com.example.feelslikemonday.R;
+import com.example.feelslikemonday.model.EmotionBookAdapter;
+import com.example.feelslikemonday.model.MoodEvent;
+import com.example.feelslikemonday.model.User;
+import com.example.feelslikemonday.ui.login.SignupActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private SharedPreferences pref;
+    private String myUserID;
+    private User currentUser;
+    private ArrayList<MoodEvent> myEmotionList = new ArrayList<>();
+    private List<MoodEvent> myCurrentMoodList;
+    private LatLng currentLocation;
+    private BitmapDescriptor markerType;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,19 +56,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        pref = getApplicationContext().getSharedPreferences(SignupActivity.PREFS_NAME, 0);
+        myUserID = pref.getString(SignupActivity.USERNAME_KEY, null);
+
+        UserDAO userDAO = new UserDAO();
+        userDAO.get(myUserID, new UserCallback() {
+            @Override
+            public void onCallback(User user) {
+                currentUser = user;
+                myEmotionList.clear();
+                myCurrentMoodList = currentUser.getMoodHistory();
+                for (int i = 0; i < myCurrentMoodList.size(); i++) {
+                    myEmotionList.add(myCurrentMoodList.get(i));
+                }
+                placeMarkers();
+            }
+        }, new VoidCallback() {
+            @Override
+            public void onCallback() {
+                Log.v("succc", "succ");
+            }
+        });
     }
+
+    private void placeMarkers() {
+        for (int i = 0; i < myEmotionList.size(); i++) {
+
+            String markerLocation = myEmotionList.get(i).getLocation();
+            switch (myEmotionList.get(i).getMoodType().getName()) {
+                case "Anger":
+                    markerType = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED);
+                    break;
+                case "Disgust":
+                    markerType = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
+                    break;
+                case "Fear":
+                    markerType = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_VIOLET);
+                    break;
+                case "Happiness":
+                    markerType = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA);
+                    break;
+                case "Sadness":
+                    markerType = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE);
+                    break;
+                case "Surprise":
+                    markerType = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW);
+                    break;
+            }
+            String[] latLongSplit = markerLocation.split(" ");
+            currentLocation = new LatLng(Double.valueOf(latLongSplit[1]), Double.valueOf(latLongSplit[0]));
+            mMap.addMarker(new MarkerOptions().position(currentLocation).icon(markerType));
+        }
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+    }
+
 }
